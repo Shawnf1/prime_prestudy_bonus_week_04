6767819b861397035b51affede55ddf54c6a674c
TShaunik <shawnf1@gmail.com>
2015-07-20 09:12:44 -0500

    First Commit
    First commit for week 4 prestudy bonus challenge

---
+++ splinter.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Shawn on 7/20/2015.
+ */
+var splinter = function (first, second)
+{
+    var z = first * second;
+    var array = [first, second, z];
+    return array;
+};
+
+console.log(splinter(1, 2));
